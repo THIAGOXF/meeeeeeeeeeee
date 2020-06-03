4af8938ff2ebdc7dfbbbdf355793a502bf0bdd3e
THIAGOXF <64556498+THIAGOXF@users.noreply.github.com>
2020-06-03 19:59:12 -0300

    Add files via upload

---
+++ EUNCONSIGOENTRARNASALAAAAAAAAAAAAA.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.n.consigo.entrar.na.salaaaaaaaaaaaaa;
+
+/**
+ *
+ * @author UsuarioLocal
+ */
+public class EUNCONSIGOENTRARNASALAAAAAAAAAAAAA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      double pernaD, pernaE, galo;
+      pernaD = 4.00;
+      pernaE = 7.00;
+      
+      System.out.println("diga quantos galos:");
+      galo galos = scan.nextDouble();
+      galo = pernaD + pernaE;
+      
+      
+}}
